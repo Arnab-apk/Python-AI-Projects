055a0fa51158cbef7d271e70bc7b9fd60ed9f165
Mr.Invictus <arnabmandal261@gmail.com>
2025-09-03 12:13:45 +0530

    done

--- image_class.py
+++ image_class.py
@@ -1,7 +1,14 @@
 #import libraries
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import datasets,layers,models
-from tensorflow.keras
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.utils import to_categorical
 
-#load the dataset
+#load the mnistdataset
+(train_images, train_labels),(test_images,test_lables)=datasets.mnist.load_data()
+
+#preprovessing: Normalise the pixel value to be between 0 and 1
+train_images=train_images/255.0
+train_images=test_images/255.0
+
+#reshape the images
